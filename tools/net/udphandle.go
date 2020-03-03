b6ee4b621d31a6c2130acc14e5d504e6a8c91a5d
叶晓兵 <865756739@qq.com>
2020-03-03 10:22:20 +0800

    udp

--- tools/net/udphandle.go
+++ tools/net/udphandle.go
@@ -1,9 +1,46 @@
 package net
 
+import (
+    "github.com/yellia1989/tex-go/tools/log"
+)
+
 type udpHandle struct {
     svr *Svr
+    conn *net.UDPConn
 }
 
 func (h *udpHandle) Run() {
-    // TODO
+    /*
+    defer func() {
+        if h.conn != nil {
+            h.conn.Close()
+        }
+    }()
+
+    addr, err := net.ResolveUDPAddr("udp4", cfg.Address)
+    if err != nil {
+        log.FErrorf("listen on %s failed", cfg.Address)
+        return
+    }
+    h.conn, err = net.ListenUDP("udp4", addr)
+    if err != nil {
+        log.FErrorf("listen on %s failed", cfg.Address)
+        return
+    }
+    log.FDebugf("start listen on:%s", addr)
+
+    tmpbuf := make([]byte, 65535)
+    for atomic.LoadInt32(&h.svr.close) == 0 {
+        n, udpAddr, err := h.conn.ReadFromUDP(tmpbuf)
+        if err != nil {
+            if isTimeoutErr(err) {
+                continue
+            } else {
+                log.FErrorf("udp read err:%s", err.Error())
+                return
+            }
+        }
+        pkg := make([]byte, n)
+        copy(pkg, tmpbuf[0:n])
+    }*/
 }
