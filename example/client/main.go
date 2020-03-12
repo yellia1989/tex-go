49f405a0bd6e3ba7ecf99d34136da99b1417c6e6
Yellia <865756738@qq.com>
2020-03-12 18:42:51 +0800

    1

--- example/client/main.go
+++ example/client/main.go
@@ -3,7 +3,6 @@ package main
 import (
     "sync"
     "time"
-    "fmt"
     "os"
     "syscall"
     "os/signal"
@@ -11,6 +10,8 @@ import (
     "github.com/yellia1989/tex-go/tools/log"
     "github.com/yellia1989/tex-go/example/server/echo"
 )
+
+var done sync.WaitGroup
 
 func main() {
     log.SetFrameworkLevel(log.INFO)
@@ -24,12 +25,17 @@ func main() {
     c := make(chan os.Signal)
     signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
     go func() {
-        <-c
-        close(closecli)
+        select {
+        case <-c:
+            close(closecli)
+        case <-time.After(time.Second * 15):
+            // 测试15秒, 统计每秒hello次数
+            close(closecli)
+        }
     }()
 
-    var done sync.WaitGroup
-    num := 10000
+    q := newQps(time.Second, closecli)
+    num := 1000
 
     for i := 0; i < num; i++ {
         done.Add(1)
@@ -42,36 +48,92 @@ func main() {
 
             echoPrx := new(echo.EchoService)
             comm.StringToProxy("test.EchoServer.EchoServiceObj@tcp -h 127.0.0.1 -p 9000 -t 3600000", echoPrx)
-            loop := time.NewTicker(time.Millisecond * 1000)
-            var total time.Duration
-            invokenum := float64(0)
             run := true
             for run {
                 select {
                 case <-closecli:
                     run = false
                     break
-                case <-loop.C:
+                default:
                     var resp string
                     start := time.Now()
-                    err := echoPrx.Hello(fmt.Sprintf("client:%d, yellia", id), &resp)
-                    total += time.Since(start)
+                    err := echoPrx.Hello("hello", &resp)
+                    q.add(time.Since(start))
                     if err != nil {
                         log.Errorf("err:%s", err)
-                    } else {
-                        invokenum++
                     }
                 }
             }
-            cost := float64(total.Milliseconds())
-            if invokenum != 0 {
-                log.Debugf("client:%d stop, cost:%.2f ms, invokenum:%.2f, ops:%.2f ms", id, cost, invokenum, cost/invokenum)
-            }
-            loop.Stop()
             done.Done()
         }(i+1)
     }
 
     done.Wait()
     log.Debug("wait client to exit")
+}
+
+type Qps struct {
+    mu sync.Mutex
+
+    total uint32
+    qps uint32
+    times uint32
+
+    qpstime time.Duration
+}
+
+func (p *Qps) add(t time.Duration) {
+    p.mu.Lock()
+    defer p.mu.Unlock()
+
+    p.qps++
+    p.qpstime += t
+}
+
+func (p *Qps) round() {
+    p.mu.Lock()
+
+    qps := p.qps
+    t := p.qpstime
+
+    p.total += p.qps
+    p.qps = 0
+    p.times++
+    
+    p.qpstime = 0
+    p.mu.Unlock()
+
+    log.Debugf("QPS:%d,time:%d ms", qps, t.Milliseconds()/int64(qps))
+}
+
+func (p *Qps) avg() {
+    p.mu.Lock()
+    defer p.mu.Unlock()
+   
+    log.Debugf("AVG QPS:%d", p.total/p.times)
+}
+
+func newQps(roundtime time.Duration, stop chan bool) *Qps {
+    loop := time.NewTicker(roundtime)
+
+    q := &Qps{}
+
+    go func(){
+        done.Add(1)
+        defer func() {
+            done.Done()
+        }()
+        for {
+            select {
+            case <-stop:
+                loop.Stop()
+                q.avg() 
+                return
+            case <-loop.C:
+                q.round()
+            }
+        }
+    }()
+
+    return q
 }
